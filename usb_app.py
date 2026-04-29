9537c4d13801f97dd78c697e2da73af6c10badd7
tsohg <tsohg777@cvijano.ch>
2026-04-29 13:22:02 +0200

    Important

---
+++ usb_app.py
@@ -0,0 +1,20 @@
+Websites = []
+print("Dober dan Wilkommen kod moj program gje je sehr speziallisiert.\n")
+
+x = input("ako hoces Snati kako ovaj programm funktionira:\nPritisni = 1\nako ne onda samo irgendeine andere zahl.\nInput:  ")
+    
+
+if x == "1":
+    print("\nEgal koja Website, ti moses uset Link i onda hinzufügen Link, i on ce tebi sa tvoje keyworte sve aktuelle nachrichten anzeigen.")
+    print("Ali sto je to nutzvoll mozes schnell selber gledat ili search per google, aliiiii ti moses ako hoces sa moj programm 10 verschiedene seiten")
+    print("Imat i svi oni per knopfdruck cu tebi schicken sve infomation zb Ukraine krieg, A sto google to nemose, sato sto ti moses egal koju webseite")
+    print("Stavit i tvoje links bleiben permanenet ti moses imat 50+ seiten/artikel i stalno odma sve infomationen dobit od egal koji thema to")
+    print("tebi manuell isto sa KI treba minimum 10-20minuten, sa moj programm 1 knopfdruck.\n\n")
+    input("Pritisni 'Enter' ako si gotov: ")
+    
+
+
+user_input = input("Molimte copy-paste tvoj link: ")
+Websites.append(user_input)
+print(f"\nWebsite od ime: '{user_input}' je Hinzugefügt u liste\n\n{Websites}")
+
