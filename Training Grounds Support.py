bb9d5c0e32f766b046c25cd369b5dcba5258ddd6
tsohg <tsohg777@cvijano.ch>
2026-05-25 16:38:26 +0200

    good

--- Training Grounds Support.py
+++
@@ -1,5 +0,0 @@
-
-
-aaah so this would mean 
-chmod u+x file
-change permission from u(owner) and give him (+) execute rights (x)?  
---
+++ TrainingGrounds.py
@@ -0,0 +1,11 @@
+var1 = int(input("ur number :"))
+var2 = int(input("ur number :"))
+
+
+def plus():
+    return var1 + var2
+
+def mal():
+    return var1 * var2
+
+print(mal())
